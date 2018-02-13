2b0e4380ef49c4a5932506940250ec12222d4961
Stephanie Sara Chong <stephaniesarachong@Stephanies-MacBook-Air-2.local>
2018-02-13 11:24:16 -0800

    Set up repo, express, jquery. Link server and client. Add form and button to index.html.

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,9 @@
+console.log('in app.js');
 
-
+$(document).ready(function() {
+	console.log('ready!');
+	$("#csvform").on('submit', (event) => {
+		console.log('submit!');
+		event.preventDefault();
+	});
+});
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,8 @@
+const express = require('express');
+const app = express();
 
+app.use(express.static('client'));
 
+app.get('/', (req, res) => res.send('Goodbye world'));
+
+app.listen(3000, () => console.log('Example app listening on port 3000'));
