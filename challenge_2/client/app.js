9cd095e3cd6f06b3b0dc4ac23b6ed0d4e6a1f36c
Stephanie Sara Chong <stephaniesarachong@Stephanies-MacBook-Air-2.local>
2018-02-13 14:15:44 -0800

    Link request between client and server with success and parsed body.

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -2,8 +2,59 @@ console.log('in app.js');
 
 $(document).ready(function() {
 	console.log('ready!');
-	$("#csvform").on('submit', (event) => {
-		console.log('submit!');
-		event.preventDefault();
-	});
+	$("#csvform").submit((event) => handleSubmit(event));
 });
+
+/*
+// test json
+{
+    "firstName": "Joshie",
+    "lastName": "Wyattson",
+    "county": "San Mateo",
+    "city": "San Mateo",
+    "role": "Broker"
+}
+
+*/
+var handleSubmit = (event) => {
+	console.log('submit!');
+	event.preventDefault();
+	var message = $("#textbox").val();
+	send(message);
+};
+
+var send = (message) => {
+	console.log(message);
+
+	// test with test object, see below
+	$.ajax({
+		method: 'POST',
+		url: 'http://127.0.0.1:3000/csv',
+		// contentType: 'json/application',
+		data: {name: 'Stephanie', location: 'SF', color: 'teal'},
+		success: () => {
+			console.log('SUCCESS');
+		},
+		error: (err) => {
+			console.log('ERROR');
+		}
+	})
+
+};
+
+
+
+//get request
+	// request('http://127.0.0.1:3000/', (err, res, body) => {
+	// 	console.log('request made');
+	// });
+
+	// 	var sendObj = {
+	// 	url: 'http://127.0.0.1:3000/',
+	// 	json: {
+	// 		testProp: 'hello here'
+	// 	},
+	// }
+	// request.post(sendObj, (err, res, body) => {
+	// 	console.log('post request send back!');
+	// });
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,8 +1,33 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
+// const router = require('express').Router()
+
+app.set('port', 3000);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true})); // IMPT!!!
+app.use(morgan('dev'));
+app.use(cors());
+
+app.get('/csv', (req, res) => {
+	console.log('GET');
+	// res.status(200).send('RETURNING GET REQUEST');
+	// res.json(results);
+	res.send('RETURNING POST REQUEST');
+});
+
+app.post('/csv', (req, res) => {
+	// res.set({'content-type': 'application/json'});
+	console.log('POST');
+	console.log('BODY', req.body);
+	res.sendStatus(201);
+});
 
 app.use(express.static('client'));
 
-app.get('/', (req, res) => res.send('Goodbye world'));
-
-app.listen(3000, () => console.log('Example app listening on port 3000'));
+app.listen(app.get('port'), () => {
+	console.log('CSV app listening on port 3000')
+});
